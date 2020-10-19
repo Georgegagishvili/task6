e209986e479fad6d882c912a659aa439222bca06
G1304 <georgegagishvili@gmail.com>
2020-10-19 20:46:43 +0400

    firsT

---
+++ database/migrations/2020_10_19_151234_books.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Books extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('author')->nullable();
+            $table->String('title')->nullable();
+            $table->text('text')->nullable();
+            $table->Integer('genre_id');
+            $table->timestamp('add_date');
+            $table->text('image')->nullable();
+        });  
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
